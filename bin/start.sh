9bfe22eff88b806d1937692086a85f55b5ae3c11
Zhian N. Kamvar <zkamvar@gmail.com>
2020-05-19 16:26:52 -0700

    hide warnings from Jekyll
    In the new Ruby, there's a warning from Jekyll about using the last
    argument as keyword parameters. I've found that others are equally
    frustrated by this and have resorted to either upgrading (not yet
    prepared to do this) or silenceing. We have gone with the latter.
    
    ref: https://github.com/jekyll/jekyll/issues/7947

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/execlineb -P
 
-exec /usr/local/bundle/bin/jekyll serve -s /home/rstudio -d /home/rstudio/_site --host 0.0.0.0
+exec /usr/local/bundle/bin/jekyll serve -s /home/rstudio -d /home/rstudio/_site --host 0.0.0.0 2>/dev/null
 
 
