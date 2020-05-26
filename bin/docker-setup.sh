b24c589992b5b8efccc2e37e36bd1ea2ed8203a3
Zhian N. Kamvar <zkamvar@gmail.com>
2020-05-26 15:46:59 -0700

    add cleanup steps

--- bin/docker-setup.sh
+++ bin/docker-setup.sh
@@ -2,38 +2,18 @@
 
 set -e
 
+# set a default password
 export PASSWORD=${PASSWORD:=data4Carp}
+
+# record the inital state of the directory so that we can remove the detritus later
+ls -1a /home/rstudio > /srv/in.txt
+
 # This creates links to the gemfiles. The beauty behind this is that the
 # residual gemfiles are not present in the directory anymore. 
-if [ -e Gemfile ]; then
+if [ -e /home/rstudio/Gemfile ]; then
   echo "exists" > /dev/null
 else
   cp -t /home/rstudio /srv/gems/Gemfile /srv/gems/Gemfile.lock
 fi
-
-
-
-echo ""
-echo ""
-echo "This is the alpha test of The Carpentries Lesson Template docker image."
-echo "Please open an issue if you find any problems or have suggestions:"
-echo "<https://github.com/carpentries/lesson-docker/issues/new>"
-echo ""
-echo "-----------------------------------------------------------------------------------"
-echo ""
-echo " ->> Open RStudio: <http://localhost:8787> <<-"
-echo " ->> Preview Site:   <http://0.0.0.0:4000> <<-"
-echo ""
-echo " Username: rstudio"
-echo " Password: ${PASSWORD}"
-echo ""
-echo "Once you are in RStudio in your browser, edit any of the files in \`_episodes_rmd/\`"
-echo "and then run \`make lesson-md\` in the Terminal tab to render the lessons. The"
-echo "website will update automatically"
-echo "-----------------------------------------------------------------------------------"
-echo ""
-echo "When you are finished, close the browser windows and use <CTRL+C> to exit"
-echo "this session."
-echo ""
 
 exec "$@"
--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/with-contenv bash
 
+echo ""
+echo ""
+echo "This is the alpha test of The Carpentries Lesson Template docker image."
+echo "Please open an issue if you find any problems or have suggestions:"
+echo "<https://github.com/carpentries/lesson-docker/issues/new>"
+echo ""
+echo "-----------------------------------------------------------------------------------"
+echo ""
+echo " ->> STEP 1 --- Preview Site: <http://0.0.0.0:4000>   <<-"
+echo " ->> STEP 2 --- Open RStudio: <http://localhost:8787> <<-"
+echo ""
+echo " Username: rstudio"
+echo " Password: ${PASSWORD}"
+echo ""
+echo "Once you are in RStudio in your browser, edit any of the files in \`_episodes_rmd/\`"
+echo "and then run \`make lesson-md\` in the Terminal tab to render the lessons. The"
+echo "website will update automatically"
+echo "-----------------------------------------------------------------------------------"
+echo ""
+echo "When you are finished, close the browser windows and use <CTRL+C> to exit"
+echo "this session."
+echo ""
+
 exec ${BUNDLE_BIN}/jekyll serve -s /home/rstudio -d /home/rstudio/_site --host 0.0.0.0 
 
